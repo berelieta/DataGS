6bdff802b45dd21c9dcdfcb76431ab4b3b6cf06f
berelieta <berelieta@gmail.com>
2021-01-22 19:03:08 -0600

    Add files via upload
    first session

---
+++ comandos.py
@@ -0,0 +1,9 @@
+conda install numpy
+conda create --name ambiente_curso
+conda env list
+conda activate ambiente_curso
+conda install numpy
+
+
+//configurar variables de entorno
+export https_proxy=http://bcedillo:Franquicia2021GS@10.50.8.20:8080
